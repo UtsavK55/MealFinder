4c2431e2dd1f7e2ddea6d8586565ea04f6749295
UtsavK55 <utsav@gkmit.co>
2024-11-26 12:56:47 +0530

    fix:extract types

--- src/components/horizontalScroll/styles.ts
+++ src/components/horizontalScroll/styles.ts
@@ -7,7 +7,6 @@ export const horizontalScrollStyles = createThemedStyles(
     const {scaleSize, moderateScale} = scalingMetrics;
     return StyleSheet.create({
       container: {
-        // marginVertical: scaleSize(30),
         flex: 1,
       },
       sectionTitle: {
