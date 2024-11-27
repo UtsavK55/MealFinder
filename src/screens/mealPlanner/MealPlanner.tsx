2cb2442ee8249c02823ec9349f5951d5558e54da
UtsavK55 <utsav@gkmit.co>
2024-11-27 14:09:13 +0530

    feat: add calendar to meal planner screen

--- src/screens/mealPlanner/MealPlanner.tsx
+++ src/screens/mealPlanner/MealPlanner.tsx
@@ -1,12 +1,28 @@
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {CalendarProvider} from 'react-native-calendars';
 
 import BaseContainer from '@components/baseContainer';
+import CalendarStrip from '@components/calendarStrip';
+import {formatDate} from '@helpers';
 
 const MealPlanner = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const onDayPress = (day: any) => {
+    const newDate = new Date(day?.dateString);
+    if (newDate.getTime()) {
+      setSelectedDate(newDate);
+    }
+  };
+
   return (
-    <BaseContainer>
-      <Text>MealPlanner</Text>
-    </BaseContainer>
+    <CalendarProvider
+      date={formatDate(selectedDate)}
+      onDateChanged={onDayPress}>
+      <BaseContainer>
+        <CalendarStrip selectedDate={selectedDate} onDayPress={onDayPress} />
+      </BaseContainer>
+    </CalendarProvider>
   );
 };
 
